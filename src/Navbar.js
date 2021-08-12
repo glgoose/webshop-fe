156dc4137a10cbbdc8009ff6adb4a9ab6f6010f8
Glenn Goossens <glgoose@pm.me>
2021-08-12 22:48:41 +0200

    add cart & change search btn to icon

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,33 +5,22 @@ function Navbar () {
         <a className='navbar-brand' href='#'>
           webshop
         </a>
-        <button
-          className='navbar-toggler'
-          type='button'
-          data-bs-toggle='collapse'
-          data-bs-target='#navbarSupportedContent'
-          aria-controls='navbarSupportedContent'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-        >
-          <span className='navbar-toggler-icon'></span>
-        </button>
-        <div
-          className='collapse navbar-collapse flex-grow-1'
-          id='navbarSupportedContent'
-        >
-          <form className='d-flex w-100'>
+        <form className='d-flex flex-grow-1'>
+          <div className='input-group'>
             <input
-              className='form-control me-2 flex-grow-1'
+              className='form-control flex-grow-1'
               type='search'
               placeholder='Search'
               aria-label='Search'
             />
-            <button className='btn btn-outline-success' type='submit'>
-              Search
+            <button className='btn btn-outline-secondary' type='submit'>
+              <i className='bi bi-search'></i>
             </button>
-          </form>
-        </div>
+          </div>
+        </form>
+        <button className='btn btn-outline-info ms-2 fw-bold'>
+          <i class='bi bi-cart'></i>
+        </button>
       </div>
     </nav>
   )
