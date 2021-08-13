bb6a28198c6b9ca388ebaa90c43852c41ff57e36
Glenn Goossens <glgoose@pm.me>
2021-08-13 14:33:05 +0200

    rm useless computed key

--- src/CardOverlay.js
+++ src/CardOverlay.js
@@ -1,12 +1,14 @@
 import Button from 'react-bootstrap/Button'
 
 function CardOverLay () {
+	const clickHandler = () => console.log('add to cart')
+
   return (
     <div
       className='overlay position-absolute bg-light w-100 h-75 top-0 start-0 d-flex justify-content-center align-items-center' 
-			style={{['--bs-bg-opacity']: '.85'}}
+			style={{'--bs-bg-opacity': '.85'}}
     >
-      <Button variant='info'> 
+      <Button variant='info' size='lg' onClick={clickHandler}> 
         <i class='bi bi-cart-plus'></i>
       </Button>
     </div>
