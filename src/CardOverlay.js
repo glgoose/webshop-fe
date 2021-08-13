5437dc67f16c7d6add725ebfda7dc2f06831782e
Glenn Goossens <glgoose@pm.me>
2021-08-13 13:16:08 +0200

    set opacity to bg-color

--- src/CardOverlay.js
+++ src/CardOverlay.js
@@ -3,9 +3,10 @@ import Button from 'react-bootstrap/Button'
 function CardOverLay () {
   return (
     <div
-      className='overlay position-absolute bg-light opacity-75 w-100 h-75 top-0 start-0 d-flex justify-content-center align-items-center'
+      className='overlay position-absolute bg-light w-100 h-75 top-0 start-0 d-flex justify-content-center align-items-center' 
+			style={{['--bs-bg-opacity']: '.85'}}
     >
-      <Button variant='info'>
+      <Button variant='info'> 
         <i class='bi bi-cart-plus'></i>
       </Button>
     </div>
